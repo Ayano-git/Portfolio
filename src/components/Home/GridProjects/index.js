1d9825723102605bd0469d42636d4782887c6c66
Ayano-git <anthonyverret2436@gmail.com>
2023-11-19 00:14:28 +0100

    prepare grid projects and add lettrine to Title

---
+++ src/components/Home/GridProjects/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.scss'
+
+const GridProjects = () => {
+  return (
+    <div className='projects-container'>GridProjects</div>
+  )
+}
+
+export default GridProjects
