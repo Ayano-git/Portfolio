4440890a7c7d24c8393af63bffc5ea0a6982e09e
Ayano-git <anthonyverret2436@gmail.com>
2023-11-18 22:23:43 +0100

    build temp layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,8 +1,22 @@
-import Sidebar from "./Sidebar";
-import "./index.scss";
+import { Outlet } from 'react-router-dom'
+import Sidebar from '../Sidebar'
+import './index.scss'
 
 const Layout = () => {
-  return <><Sidebar></Sidebar></>
+  return (
+    <div className="App">
+      <Sidebar />
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+        <Outlet />
+        <div>
+          <span className="tags bottom-tags">&lt;/body&gt;</span>
+          <br />
+          <span className="tags bottom-tag-html">&lt;/html&gt;</span>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Layout
