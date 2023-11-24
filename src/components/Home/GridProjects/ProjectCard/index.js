4addc7dd1c7761f73c0bf10cf151aee128e47561
Ayano-git <anthonyverret2436@gmail.com>
2023-11-24 03:07:08 +0100

    fix link sur les project-cards

--- src/components/Home/GridProjects/ProjectCard/index.js
+++ src/components/Home/GridProjects/ProjectCard/index.js
@@ -3,10 +3,10 @@ import React from 'react'
 import './index.scss'
 import { Link, NavLink } from 'react-router-dom'
 
-const ProjectCard = ({ projectImageSrc, projectTitle, projectAlt }) => {
+const ProjectCard = ({ projectImageSrc, projectTitle, projectAlt, projectLink }) => {
   return (
     <div className="project-card-container">
-      <NavLink className="project-card" to="/">
+      <NavLink className="project-card" to={projectLink}>
         <img src={projectImageSrc} alt={projectAlt} />
         <h3>{projectTitle}</h3>
       </NavLink>
